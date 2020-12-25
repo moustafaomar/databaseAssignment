85d41218c284cb2349346185b7d0802a307bb5a8
moustafaomar <moustafaomar200@gmail.com>
2020-12-25 14:55:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ mydb = mysql.connector.connect(
     database="databasesubmissions"
     )
 mycursor = mydb.cursor()
+mycursor.execute('set max_allowed_packet=67108864')
 mycursor.execute("CREATE TABLE Doctors (name VARCHAR(255),department VARCHAR(255), id INT, PRIMARY KEY(id))")
 mycursor.execute("CREATE TABLE Patients (name VARCHAR(255), id INT, PRIMARY KEY(id))")
 mycursor.execute("CREATE TABLE DOC_PAT ( D_code INT, P_code INT, FOREIGN KEY (P_code) REFERENCES Patients(id),FOREIGN KEY (D_code) REFERENCES Doctors(id))")
