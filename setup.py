6c32713024001c10a0692857deef859b719da02d
moustafaomar <moustafaomar200@gmail.com>
2020-12-25 16:17:38 +0200

    Fixed DB name

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ mycursor = mydb.cursor()
 
 mycursor.execute("CREATE TABLE doctors (name VARCHAR(255),department VARCHAR(255), id INT, PRIMARY KEY(id))")
 mycursor.execute("CREATE TABLE patients (name VARCHAR(255), id INT, PRIMARY KEY(id))")
-mycursor.execute("CREATE TABLE doc_pat ( D_code INT, P_code INT, FOREIGN KEY (P_code) REFERENCES Patients(id),FOREIGN KEY (D_code) REFERENCES Doctors(id))")
+mycursor.execute("CREATE TABLE doc_pat (D_code INT, P_code INT, FOREIGN KEY (P_code) REFERENCES patients(id),FOREIGN KEY (D_code) REFERENCES doctors(id))")
